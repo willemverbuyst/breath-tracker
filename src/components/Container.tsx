446855557d0095f8851e4eef3c6d3569b3d62b15
Willem Verbuyst <willemverbuyst@gmail.com>
2022-11-05 11:26:15 +0100

    feat: add clock

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,29 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styles from './styles.module.css'
 
 enum TimerState {
   Start = 'start',
   Stop = 'stop',
   Reset = 'reset',
+}
+
+interface IProps {
+  num: number
+}
+
+const Clock = ({ num }: IProps) => {
+  const [time, setTime] = useState('00:00')
+
+  useEffect(() => {
+    const minutes = Math.floor(num / 60)
+
+    const min = minutes < 10 ? `0${minutes}` : `${minutes}`
+    const sec = num % 60 < 10 ? `0${num % 60}` : `${num % 60}`
+
+    setTime(`${min}:${sec}`)
+  }, [num])
+
+  return <div className={styles.clock}>{time}</div>
 }
 
 export default function Container() {
@@ -40,7 +59,7 @@ export default function Container() {
   return (
     <div>
       <button onClick={handleTimer}>{stateTimer}</button>
-      <div>{duration}</div>
+      <Clock num={duration} />
       <div
         className={`${styles.container} ${
           stateTimer === TimerState.Start ? styles.breathing : ''
